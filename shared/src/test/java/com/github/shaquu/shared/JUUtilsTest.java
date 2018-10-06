be1b9ecad2106f19677cb4c1e81855511af7c5f8
Shaquu <tadev3@gmail.com>
2018-10-06 22:14:07 +0200

    Imports changes in JUUtilsTest

--- shared/src/test/java/com/github/shaquu/shared/JUUtilsTest.java
+++ shared/src/test/java/com/github/shaquu/shared/JUUtilsTest.java
@@ -9,7 +9,8 @@ package com.github.shaquu.shared;
 import com.github.shaquu.shared.packet.BaseData;
 import com.github.shaquu.shared.packet.LogonData;
 import com.github.shaquu.shared.packet.MessageData;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,29 +19,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.DataFormatException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 class JUUtilsTest {
 
     private final static Logger logger = Logger.getLogger(JUUtilsTest.class.getName());
 
-    @org.junit.jupiter.api.BeforeAll
+    @BeforeAll
     static void setUp() {
         new JUUtils();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getGlobalIp() {
         logger.log(Level.INFO, "Starting test getGlobalIp");
 
         try {
             final String globalIp = JUUtils.getGlobalIp();
-            Assertions.assertEquals("104.248.142.11", globalIp, "Global IP is not correct.");
+            assertEquals("104.248.142.11", globalIp, "Global IP is not correct.");
         } catch (JUUtilsException e) {
             logger.log(Level.WARNING, e.getMessage());
-            Assertions.fail("Failed while getting global IP");
+            fail("Failed while getting global IP");
         }
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void compressLogonData() {
         logger.log(Level.INFO, "Starting test compressLogonData");
 
@@ -49,14 +53,14 @@ class JUUtilsTest {
         try {
             byte[] logonBytes = JUUtils.compress(BaseData.getBytes(logonData));
             LogonData logonObject = (LogonData) BaseData.getData(JUUtils.decompress(logonBytes));
-            Assertions.assertEquals(logonData, logonObject, "Object is not the same.");
+            assertEquals(logonData, logonObject, "Object is not the same.");
         } catch (IOException | ClassNotFoundException | DataFormatException e) {
             logger.log(Level.WARNING, e.getMessage());
-            Assertions.fail("Failed while compressing logon data");
+            fail("Failed while compressing logon data");
         }
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void compressMessageData() {
         logger.log(Level.INFO, "Starting test compressMessageData");
 
@@ -65,14 +69,14 @@ class JUUtilsTest {
         try {
             byte[] messageBytes = JUUtils.compress(BaseData.getBytes(messageData));
             MessageData messageObject = (MessageData) BaseData.getData(JUUtils.decompress(messageBytes));
-            Assertions.assertEquals(messageData, messageObject, "Object is not the same.");
+            assertEquals(messageData, messageObject, "Object is not the same.");
         } catch (IOException | ClassNotFoundException | DataFormatException e) {
             logger.log(Level.WARNING, e.getMessage());
-            Assertions.fail("Failed while compressing message data");
+            fail("Failed while compressing message data");
         }
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void splitMessage() {
         logger.log(Level.INFO, "Starting test splitMessage");
 
@@ -84,10 +88,10 @@ class JUUtilsTest {
 
         List<String> msgSplitted = JUUtils.splitMessage(msg, 5);
 
-        Assertions.assertEquals(msgSplittedTest.size(), msgSplitted.size(), "Message splitted to bad size");
+        assertEquals(msgSplittedTest.size(), msgSplitted.size(), "Message splitted to bad size");
 
         for (int i = 0; i < msgSplitted.size(); i++) {
-            Assertions.assertEquals(msgSplittedTest.get(i), msgSplitted.get(i), "Splitted message do not match");
+            assertEquals(msgSplittedTest.get(i), msgSplitted.get(i), "Splitted message do not match");
         }
     }
 }
