02fd94c3121409ee8a65b9bcf8f5062c254bf980
Shaquu <tadev3@gmail.com>
2018-10-09 19:51:28 +0200

    Fixed JUClientTest to stale test while waiting for input.

--- client/src/test/java/com/github/shaquu/client/JUClientTest.java
+++ client/src/test/java/com/github/shaquu/client/JUClientTest.java
@@ -12,6 +12,11 @@
 
 package com.github.shaquu.client;
 
+import com.github.shaquu.shared.JUUtils;
+import com.github.shaquu.shared.JUUtilsException;
+import com.github.shaquu.shared.prefs.JUPrefs;
+import com.github.shaquu.shared.prefs.JUPrefsException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.logging.Level;
@@ -27,6 +32,15 @@ class JUClientTest {
 
     private final static Logger logger = Logger.getLogger(JUClientTest.class.getName());
 
+    @BeforeAll
+    static void setUp() {
+        try {
+            new JUPrefs();
+        } catch (JUPrefsException e) {
+            fail(e.getMessage());
+        }
+    }
+
     /**
      * Constructor test.
      */
@@ -39,8 +53,40 @@ class JUClientTest {
         assertNotNull(client);
 
         try {
+            writeClientInput();
             JUClient.main(new String[]{});
+            removeClientInput();
         } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void getPrefFromKeyboard() {
+        writeClientInput();
+        try {
+            JUUtils.getPrefFromKeyboard("clientName", "Enter your username: ", 0, JUPrefs.Type.STRING, null);
+            JUUtils.getPrefFromKeyboard("ip", "Enter server ip [leave empty for default]: ", 0, JUPrefs.Type.STRING, JUUtils.getGlobalIp());
+        } catch (JUPrefsException | JUUtilsException e) {
+            fail(e.getMessage());
+        }
+        removeClientInput();
+    }
+
+    private void writeClientInput() {
+        try {
+            JUPrefs.write("clientName", "clientName", JUPrefs.Type.STRING);
+            JUPrefs.write("ip", "127.0.0.1", JUPrefs.Type.STRING);
+        } catch (JUPrefsException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private void removeClientInput() {
+        try {
+            JUPrefs.remove("clientName");
+            JUPrefs.remove("ip");
+        } catch (JUPrefsException e) {
             fail(e.getMessage());
         }
     }
